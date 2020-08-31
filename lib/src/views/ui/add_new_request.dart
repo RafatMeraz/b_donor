8725c0ec688fbd294b99f948849aa69f483aeb96
Rafat Jamader Meraz <razhossain201@gmail.com>
2020-09-01 00:42:58 +0600

    add a new blood request ui done

--- lib/src/views/ui/add_new_request.dart
+++ lib/src/views/ui/add_new_request.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:group_button/group_button.dart';
 import 'package:organize_flutter_project/src/views/utils/contants.dart';
 import 'package:organize_flutter_project/src/views/utils/reusable_widgets.dart';
 
@@ -8,6 +9,10 @@ class AddNewRequest extends StatefulWidget {
 }
 
 class _AddNewRequestState extends State<AddNewRequest> {
+  String _donorBloodGroup = 'A+';
+
+  final bloodGroups = ["A+", "A-", "B-", "B+", "O+", "O-", "AB-", "AB+"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,26 +23,67 @@ class _AddNewRequestState extends State<AddNewRequest> {
         leading: BackButton(color: kPurpleColor),
         title: Text('Request for Blood', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: kBlackColor)),
       ),
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 20),
-            Text('Address Preference', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: kBlackColor)),
-            SizedBox(height: 10),
-            RoundedTextField(
-              controller: null,
-              hint: 'Address',
-            ),
-            SizedBox(height: 20),
-            Text('Blood for', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: kBlackColor)),
-            SizedBox(height: 10),
-            RoundedTextField(
-              controller: null,
-              hint: 'Blood for',
-            )
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 20),
+              Text('Address Preference', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: kBlackColor)),
+              SizedBox(height: 10),
+              RoundedTextField(
+                controller: null,
+                hint: 'Address',
+              ),
+              SizedBox(height: 20),
+              Text('Blood for', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: kBlackColor)),
+              SizedBox(height: 10),
+              RoundedTextField(
+                controller: null,
+                hint: 'Blood for',
+              ),
+              SizedBox(height: 20),
+              Text('Select Blood Group',
+                  style:
+                  TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+              SizedBox(height: 15),
+              GroupButton(
+                spacing: 5,
+                isRadio: true,
+                direction: Axis.horizontal,
+                onSelected: (index, isSelected) =>
+                _donorBloodGroup = bloodGroups[index],
+                buttons: bloodGroups,
+                selectedTextStyle: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+                unselectedTextStyle: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14,
+                  color: Colors.black,
+                ),
+                selectedColor: kPurpleColor,
+                unselectedColor: kWhiteColor,
+                selectedBorderColor: kPurpleColor,
+                unselectedBorderColor: Colors.grey[300],
+                borderRadius: BorderRadius.circular(0),
+                selectedShadow: <BoxShadow>[
+                  BoxShadow(color: Colors.transparent)
+                ],
+                unselectedShadow: <BoxShadow>[
+                  BoxShadow(color: Colors.transparent)
+                ],
+              ),
+              SizedBox(height: 40),
+              RoundedGradientColorButton(
+                text: 'SUBMIT',
+                onTap: (){},
+              )
+            ],
+          ),
         ),
       ),
     );
